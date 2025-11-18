32f41a82ef5c045cbb13055c8868bdea2c469a1f
Carson <carson_lu@outlook.com>
2025-11-17 18:18:05 -0800

    Added test_job

--- job-script.sh
+++ job-script.sh
@@ -4,5 +4,8 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --mail-user=clu56@student.ubc.ca
 #SBATCH --mail-type=ALL
+#SBATCH --cpus-per-task=8             # match dataloader workers
+#SBATCH --gpus-per-node=a100:1        # fill the node’s GPUs
+#SBATCH --mem=64G
 
-python main.py
+python test_job.py
